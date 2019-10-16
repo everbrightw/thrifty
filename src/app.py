404e0e676b07500318aad7b2f3e4f830d45a7bdd
yusenw2 <yusenw2@illinois.edu>
2019-10-15 22:13:03 -0500

    test pull

--- src/app.py
+++ src/app.py
@@ -5,11 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+
     return "Server is running"
 
 
 @app.route('/sample', methods=['GET'])
 def sample_function(p):
+    return 'sample function'
+
+@app.route('/name', methods=['GET'])
+def get_name(p):
     return 'sample function'
 
 
