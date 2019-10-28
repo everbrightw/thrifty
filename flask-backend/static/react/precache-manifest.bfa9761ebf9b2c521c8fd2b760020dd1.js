68f555fc2d8b304b3ed1d5d0cc81fcc4a1a88be8
yusenw2 <yusenw2@illinois.edu>
2019-10-28 16:59:43 -0500

    front end back end connected

---
+++ flask-backend/static/react/precache-manifest.bfa9761ebf9b2c521c8fd2b760020dd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dabc7c17edfa0d747a93f77d2e20ee4",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "2b44d351b93b73ff1ef8",
+    "url": "/static/react/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "92b1125301498f0f2b59",
+    "url": "/static/react/js/2.084cf5e0.chunk.js"
+  },
+  {
+    "revision": "2b44d351b93b73ff1ef8",
+    "url": "/static/react/js/main.9e35e726.chunk.js"
+  },
+  {
+    "revision": "4a5017080316628fd930",
+    "url": "/static/react/js/runtime-main.24f57757.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/react/media/logo.25bf045c.svg"
+  }
+]);
--- react-frontend/config/webpack.config.js
+++ react-frontend/config/webpack.config.js
@@ -525,7 +525,7 @@ module.exports = function(webpackEnv) {
           {
             inject: true,
             template: paths.appHtml,
-            filename: path.resolve("../../templates/index.html")
+            filename: "../../templates/index.html",
           },
           isEnvProduction
             ? {
